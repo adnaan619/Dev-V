ba4b4e2d9d37ae288d6ff66ca861e5ac61dd4b9e
Rusini Thara <72293107+rusini666@users.noreply.github.com>
2022-02-04 07:46:02 +0530

    Added blank pages

--- Mobile_App/demo_cuticare/lib/home.dart
+++ Mobile_App/demo_cuticare/lib/home.dart
@@ -1,4 +1,3 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
@@ -18,47 +17,16 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int index = 2;
-  final screens = [
-    Center(child: Text('Community', style: TextStyle(fontSize:72))),
-    Center(child: Text('Result', style: TextStyle(fontSize:72))),
-    Center(child: Text('Home', style: TextStyle(fontSize:72))),
-    Center(child: Text('Results', style: TextStyle(fontSize:72))),
-    Center(child: Text('Profile', style: TextStyle(fontSize:72)))
-  ];
 
   @override
   Widget build(BuildContext context) {
-    final items = <Widget>[
-      Icon(Icons.home, size:30),
-      Icon(Icons.group_rounded, size:30),
-      Icon(Icons.location_on, size:30),
-      Icon(Icons.assignment, size:30),
-      Icon(Icons.person_sharp, size:30),
-    ];
-    return Scaffold(
-      extendBody: true,
-      backgroundColor: Colors.blue[200],
-      appBar: AppBar(
-        elevation: 0
-      ),
-      body: Center(
-        child: Text(
-          '$index',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize:120,
-            fontWeight:  FontWeight.bold
-          )
-        )
-      ),
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: Colors.transparent,
-        height: 60,
-        index: index,
-        items: items,
-        onTap: (index) => setState(() => this.index = index),
-      )
+    return SafeArea(
+      top: false,
+        child: Center(
+          child: Scaffold(
+            Icon(icon:Icons.camera)
+          ),
+        ),
     );
   }
 }
