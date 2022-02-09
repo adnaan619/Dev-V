532731369f012a7c2eb705ecb27276d874e3e0c8
Rusini Thara <72293107+rusini666@users.noreply.github.com>
2022-02-09 10:05:19 +0530

    Changed pubspec.yaml file

--- Mobile_App/demo_cuticare/lib/main_pages/home.dart
+++ Mobile_App/demo_cuticare/lib/main_pages/home.dart
@@ -1,14 +1,18 @@
+import 'dart:io';
+
 import 'package:demo_cuticare/main_pages/profile.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() => runApp(MaterialApp(
     home: Home(
-        // initialRoute: '/', //overriding the default route
-        // routes:{
-        //   '/': (context) => Loading(),
-        //   '/home': (context) => Home(),
-        //   '/location': (context) => ChooseLocation()
-        // }
+      // initialRoute: '/', //overriding the default route
+      // routes:{
+      //   '/': (context) => Loading(),
+      //   '/home': (context) => Home(),
+      //   '/location': (context) => ChooseLocation()
+      // }
     )
 ));
 
@@ -18,36 +22,66 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  File? image;
 
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      top: false,
-        child: Center(
-          child: Scaffold(
-              body: SizedBox(
-                height: 400,
-                width: 400,
-                child: InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context)=> Profile())
-                    );
-                  }, // Image tapped
-                  splashColor: Colors.white10, // Splash color over image
-                  child: Ink.image(
-                    fit: BoxFit.cover, // Fixes border issues
-                    width: 80,
-                    height: 80,
-                    image: AssetImage(
-                        'assets/camera-icon.png',
+  // Future pickImage(ImageSource source) async {
+  //   try {
+  //     final image = await ImagePicker().pickImage(source: source);
+  //     if (image == null) return;
+  //     final imageTemporary = File(image.path);
+  //     setState(() => this.image = imageTemporary);
+  //     MaterialPageRoute(
+  //       builder: (context) =>
+  //           Profile()
+  //     );
+  //   } on PlatformException catch (e) {
+  //     print('Failed to pick image:  $e');
+  //   }
+  // }
+  pickImage(ImageSource source) async {
+    final navigator = Navigator.of(context);
+    File pickedImage = await ImagePicker().pickImage(source: source) as File;
+    if (pickedImage != null) {
+      print(pickedImage.path);
+      await navigator.push(
+          MaterialPageRoute(
+              builder: (context) =>
+                  Profile()
+          ),
+      );
+
+    }
+  }
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+          body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: InkWell(
+                    onTap: () => pickImage(ImageSource.gallery), // Image tapped
+                    child: Image.asset(
+                      'assets/camera-icon.png',
+                      fit: BoxFit.cover, // Fixes border issues
+                      width: 250.0,
+                      height: 250.0,
                     ),
                   ),
+                ),
+                SizedBox(height: 50),
+                InkWell(
+                  onTap: () => pickImage(ImageSource.camera), // Image tapped // Image tapped
+                  child: Image.asset(
+                    'assets/camera-icon.png',
+                    fit: BoxFit.cover, // Fixes border issues
+                    width: 250.0,
+                    height: 250.0,
+                  ),
                 )
-              )
-            ),
-          ),
-    );
+              ])
+      );
+    }
   }
-}
+
+
