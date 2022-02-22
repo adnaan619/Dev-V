94a63c762dd9d21bb87f5a84503d9ead416b7118
adnaan619 <86790106+adnaan619@users.noreply.github.com>
2022-02-22 12:05:05 -0800

    Main method added and debug banner removed

---
+++ lib/t&c.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:demo_cuticare/main_pages/bottom_nav_bar.dart';
+
+void main() => runApp(MaterialApp(
+  home: TermsAndConditions(),
+  debugShowCheckedModeBanner: false,
+));
