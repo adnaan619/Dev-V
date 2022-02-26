3f6e433a7b365f811eda1a13febe6b743aa12aae
adnaan619 <86790106+adnaan619@users.noreply.github.com>
2022-02-26 03:13:00 -0800

    Changes made to android and ios files in the project

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyA0johRPWemk9Sv_DyYCe-NVFzHm0xR8C0")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,6 +1,20 @@
 class Search extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height/3,
+            width:  MediaQuery.of(context).size.width,
+            child: GoogleMap(
+              initialCameraPosition:
+                  CameraPosition(target: latlng(), zoom: 16.0),
+              zoomGesturesEnabled: true,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
