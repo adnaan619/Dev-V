c23a8f336c3a81cb35ccfaa6aae306145a70e586
adnaan619 <86790106+adnaan619@users.noreply.github.com>
2022-03-01 00:11:34 -0800

    geolocator to get users location added

--- lib/location.dart
+++ lib/location.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return materialApp(
-      title: 'Parking App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return FutureProvider(
+
+      child: MaterialApp(
+        title: 'Parking App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Search(),
       ),
-      home: Search(),
+    );
+  }
+}
+
+class Search extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height/3,
+            width:  MediaQuery.of(context).size.width,
+            child: GoogleMap(
+              initialCameraPosition:
+              CameraPosition(target: LatLng(29.9792, 31.1342), zoom: 16.0),
+              zoomGesturesEnabled: true,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
