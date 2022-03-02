cabc8a0147a3037d9a2afbf2f13307c78a405a29
adnaan619 <86790106+adnaan619@users.noreply.github.com>
2022-03-01 23:38:58 -0800

    Used psotman to get the request for the closest hospitals to the user

--- lib/location.dart
+++ lib/location.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:geolocator/geolocator.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
+  final locatorService = GeoLocatorService();
   @override
   Widget build(BuildContext context){
     return FutureProvider(
-
+      create: (context) => locatorService.getLocation(),
       child: MaterialApp(
         title: 'Parking App',
         theme: ThemeData(
@@ -23,20 +26,34 @@ class MyApp extends StatelessWidget{
 class Search extends StatelessWidget{
   @override
   Widget build(BuildContext context){
+    final currentPosition = Provider.of<Position>(context);
+
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: MediaQuery.of(context).size.height/3,
-            width:  MediaQuery.of(context).size.width,
-            child: GoogleMap(
-              initialCameraPosition:
-              CameraPosition(target: LatLng(29.9792, 31.1342), zoom: 16.0),
-              zoomGesturesEnabled: true,
-            ),
+      body: (currentPosition != null)
+          ? Column(
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.height/3,
+                width:  MediaQuery.of(context).size.width,
+                child: GoogleMap(
+                  initialCameraPosition: CameraPosition(
+                      target: LatLng(currentPosition.latitude, currentPosition.longitude), zoom: 16.0),
+                  zoomGesturesEnabled: true,
+                ),
+              ),
+            ],
+          )
+          : Center(
+        child: CircularProgressIndicator(),
           ),
-        ],
-      ),
     );
+  }
+}
+
+class GeoLocatorService{
+
+  Future<Position> getLocation() async {
+    var geolocator = Geolocator();
+    return await geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high, locationPermissionLevel: GeolocationPermission.location);
   }
 }
